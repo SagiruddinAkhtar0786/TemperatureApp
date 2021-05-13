628d85123470131b7a0b233a3601a025eaf6e9c9
SagiruddinAkhtar0786 <sagir.akhtar387@gmail.com>
2021-05-13 15:45:32 +0530

    first commit

--- src/components/TempApp.js
+++ src/components/TempApp.js
@@ -6,7 +6,7 @@ const TempApp = () => {
 
     useEffect(() => {
         const fetchApi = async ()=> {
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=d6e31444beb8a24973ae569cc674e1d5`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=d6e31444beb8a24973ae569cc674e1d5`;
             const response = await fetch(url);
             const resjson = await response.json();
             // console.log(resjson);
